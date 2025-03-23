91e618ba2becbb9141d14bb3aa26c4d7cc5ff1f8
endpt <endpt_github@saberzohir.com>
2025-03-23 16:19:26 -0400

    Update path

---
+++ viewer-config.js
@@ -0,0 +1,13 @@
+window.PDFViewerApplicationOptions = {
+  disablePreferences: true,
+  disableDownload: true,
+  disablePrint: true,
+  disableOpenFile: true,
+  disablePageLabels: true,
+  toolbar: {
+    download: false,
+    print: false,
+    save: false,
+    openFile: false
+  }
+};
